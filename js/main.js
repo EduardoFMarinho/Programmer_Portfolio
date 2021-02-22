eb59c73c6dafb81e0f8d4eaef1891fb29a4345e7
EDUARDO FRANCA MARINHO <67811267+EduardoFMarinho@users.noreply.github.com>
2021-02-21 23:29:00 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -298,14 +298,14 @@ function bodyScrollingToggle() {
 /* Hide all sections except active */
 
 /* Immediatly invoked function expression - using arrow function */
-(() => {
+// (() => {
 
-    const sections = document.querySelectorAll(".section");
-    sections.forEach((section) => {
-        if(!section.classList.contains("active")){
-            section.classList.add("hide");
-        }
-    })
+//     const sections = document.querySelectorAll(".section");
+//     sections.forEach((section) => {
+//         if(!section.classList.contains("active")){
+//             section.classList.add("hide");
+//         }
+//     })
 
-})();
+// })();
 
